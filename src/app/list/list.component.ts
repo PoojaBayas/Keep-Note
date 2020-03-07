90d3dba7f88f2bf7ce2e70a6961d472864e6ea80
PoojaBayas <pooja.bayas32@gmail.com>
2020-03-07 12:02:31 -0800

    did some formating

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -11,7 +11,6 @@ import { FormControl, Validators } from "@angular/forms";
 })
 export class ListComponent implements OnInit {
   checkedItems = [];
-
   checkedItem: string;
   task: string;
   tasks = [];
@@ -20,16 +19,17 @@ export class ListComponent implements OnInit {
 
   delete(deleteItem) {
     const index = this.tasks.findIndex(item => item.name === deleteItem);
-
     this.tasks.splice(index, 1);
   }
-
+ 
+  //MatCheckboxChange {checked,MatCheckbox}
   onClick() {
     if (this.task.length > 2) {
       this.tasks.push({ name: this.task });
       this.task = "";
     }
   }
+  //MatCheckboxChange {indterminate,MatCheckbox},checkedItem Display state change
   OnChange(listItem, $event) {
     if ($event.checked == true) {
       const index = this.tasks.findIndex(item => item.name === listItem);
@@ -39,7 +39,7 @@ export class ListComponent implements OnInit {
       document.getElementById("CheckListBlock").style.display = "block";
     }
 
-    //MatCheckboxChange {checked,MatCheckbox}
+    
   }
   OnindeterminateChange(listItem, $event) {
     if ($event.checked == false) {
