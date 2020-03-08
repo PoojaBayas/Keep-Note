cdbe44110b256726c9a7db9de3c3303f0c195e11
PoojaBayas <pooja.bayas32@gmail.com>
2020-03-07 22:22:32 -0800

    Done almost functionality

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -14,33 +14,35 @@ export class ListComponent implements OnInit {
   checkedItem: string;
   task: string;
   tasks = [];
+  errMsg: string = "";
 
-  inputName = new FormControl("", [Validators.minLength(3)]);
-
-  delete(deleteItem) {
-    const index = this.tasks.findIndex(item => item.name === deleteItem);
-    this.tasks.splice(index, 1);
-  }
- 
-  //MatCheckboxChange {checked,MatCheckbox}
-  onClick() {
-    if (this.task.length > 2) {
+  //Listing the input
+  onAddItem() {
+    if (this.task && this.task.length >= 1) {
       this.tasks.push({ name: this.task });
       this.task = "";
+      this.errMsg = "";
+    } else {
+      this.errMsg = "Please enter your note";
     }
   }
-  //MatCheckboxChange {indterminate,MatCheckbox},checkedItem Display state change
+  //CheckedItem Display in checked List(bottom  content)
   OnChange(listItem, $event) {
     if ($event.checked == true) {
       const index = this.tasks.findIndex(item => item.name === listItem);
 
       this.checkedItems.push(listItem);
       this.tasks.splice(index, 1);
-      document.getElementById("CheckListBlock").style.display = "block";
+      document.getElementById("CheckListBlock").style.display = "flex";
     }
-
-    
   }
+  //delete Unchecked Item (top content)
+  deleteUncheckedItem(deleteItem) {
+    const index = this.tasks.findIndex(item => item.name === deleteItem);
+    this.tasks.splice(index, 1);
+  }
+
+  // Unchecked Items  put back to top list
   OnindeterminateChange(listItem, $event) {
     if ($event.checked == false) {
       const index = this.checkedItems.findIndex(item => item === listItem);
@@ -50,6 +52,13 @@ export class ListComponent implements OnInit {
       this.checkedItems.splice(index, 1);
     }
   }
+
+  //delete Checked Item
+  deleteCheckedItem(deleteItem) {
+    const index = this.tasks.findIndex(item => item.name === deleteItem);
+    this.checkedItems.splice(index, 1);
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
